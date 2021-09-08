6eb0fe80b1b931516e0df9510f91dde8f267e6bb
zztkm <takumi.basket1682@gmail.com>
2021-09-09 01:11:30 +0900

    iikanzi

--- gen-wav-file.py
+++ gen-wav-file.py
@@ -1,3 +1,6 @@
+import librosa
+import numpy as np
+
 from save_wav import wave_to_file
 from wave_adder import WaveAdder
 from sin_oscillator import SineOscillator
@@ -5,6 +8,22 @@ from triangle_oscillator import TriangleOscillator
 from sawtooth_oscillator import SawtoothOscillator
 from square_oscillator import SquareOscillator
 
+
+def get_val(osc, sample_rate=44100):
+    return [next(osc) for i in range(sample_rate)]
+
+
+def get_seq(osc, notes=["C4", "E4", "G4"], note_lens=[0.5, 0.5, 0.5]):
+    samples = []
+    osc = iter(osc)
+    for note, note_len in zip(notes, note_lens):
+        osc.freq = librosa.note_to_hz(note)
+        for _ in range(int(44100 * note_len)):
+            samples.append(next(osc))
+    return samples
+
+
+# NOTE: そのうち関数にしてすでにファイルが作りたいやつを指定できるようにする
 gen = WaveAdder(
     SineOscillator(freq=440),
     TriangleOscillator(freq=220, amp=0.8),
@@ -15,3 +34,47 @@ gen = WaveAdder(
 iter(gen)
 wav = [next(gen) for _ in range(44100 * 7)]
 wave_to_file(wav, fname="prelude_one.wav")
+
+dur = 10
+osc = WaveAdder(
+    SineOscillator(librosa.note_to_hz("A2")),
+    SineOscillator(librosa.note_to_hz("A2") + 3),
+    TriangleOscillator(librosa.note_to_hz("A4"), amp=0.6),
+    TriangleOscillator(librosa.note_to_hz("E5"), amp=0.8),
+)
+wav = get_val(iter(osc), 44100 * dur)
+
+osc2 = WaveAdder(
+    SineOscillator(librosa.note_to_hz("A2")),
+    SineOscillator(librosa.note_to_hz("A2") + 3),
+    TriangleOscillator(librosa.note_to_hz("C5"), amp=0.8),
+    TriangleOscillator(librosa.note_to_hz("F5"), amp=0.6),
+)
+wav2 = get_val(iter(osc2), 44100 * dur)
+
+
+wave_to_file(wav, wav2, fname="a_min6.wav")
+
+dur = 10
+s1 = ["C4", "E4", "G4", "B4"] * dur
+l1 = [0.25, 0.25, 0.25, 0.25] * dur
+
+s2 = ["C3", "E3", "G3"] * dur
+l2 = [0.333334, 0.333334, 0.333334] * dur
+
+s3 = ["C2", "G2"] * dur
+l3 = [0.5, 0.5] * dur
+
+wavl = (
+    np.array(get_seq(TriangleOscillator(amp=0.8), notes=s1, note_lens=l1))
+    + np.array(get_seq(SineOscillator(), notes=s2, note_lens=l2))
+    + np.array(get_seq(TriangleOscillator(amp=0.4), notes=s3, note_lens=l3))
+)
+
+wavr = (
+    np.array(get_seq(TriangleOscillator(amp=0.8), notes=s1[::-1], note_lens=l1))
+    + np.array(get_seq(SineOscillator(), notes=s2[::-1], note_lens=l2))
+    + np.array(get_seq(TriangleOscillator(amp=0.4), notes=s3[::-1], note_lens=l3))
+)
+
+wave_to_file(wavl, wavr, fname="c_maj7.wav")
